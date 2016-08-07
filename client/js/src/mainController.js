26c6d78fb208b7ee04285c77ef017b607e5b560f
Ben S <allobon@gmail.com>
2016-08-07 22:34:46 +0100

    google user sign in and creation

--- client/js/src/mainController.js
+++ client/js/src/mainController.js
@@ -1,5 +1,5 @@
 function mainController($scope, $firebaseObject, $firebaseAuth){
-	$scope.isLoggedIn = false;
+	this.isLoggedIn = false;
 
 	this.fbConfig = {
 		apiKey: "AIzaSyBNAnbMr3msYaa26BHm0OeVl0pMKs1eFyY",
@@ -11,26 +11,63 @@ function mainController($scope, $firebaseObject, $firebaseAuth){
 	// create a 'reference' to my own endpoint
 	firebase.initializeApp(this.fbConfig);
 
-	$scope.userDetails = null;
+	this.userDetails = null;
+	this.todoTitle = null;
 	$scope.auth = $firebaseAuth();
 
 	this.signIn = () => {
-		console.log("did i fire???");
-		$scope.auth.$signInAnonymously().then((user) => {
-			console.log("did this fire???");
-			console.log($scope.isLoggedIn);
-			console.log($scope.userDetails);
-			this.isLoggedIn = true;
+		$scope.auth.$signInWithPopup("google")
+		.then((user) => {
+			console.log(user)
 			this.userDetails = user;
-			console.log($scope.isLoggedIn);
-			console.log($scope.userDetails);
-		});
+			this.isLoggedIn = true;
+			this.createUser();
+		})
+		.catch((error) => {
+			console.log(error)
+		})
 	}
 
 	this.signOut = () => {
 		$scope.auth.$signOut();
 		this.isLoggedIn = false;
 		this.userDetails = null;
+	}
+
+	this.createUser = () => {
+		console.log("hello!");
+		let ref = firebase.database().ref("profiles/" + this.userDetails.user.uid).set({
+			fullName: this.userDetails.user.displayName,
+			email: this.userDetails.user.email,
+			photo : this.userDetails.user.photoURL
+		})
+		console.log(ref);
+
+		/*
+		let obj = $firebaseObject(ref);
+
+		obj.$save().then((res) => {
+			console.log("worked?");
+			console.log(res);
+		}).catch((e) => {
+			console.log("no");
+			console.log(e);
+		})
+		*/
+	}
+
+	this.submitTodo = () => {
+		if (this.todoTitle){/*
+			let ref = firebase.database().ref("todos").push();
+			let profileRef = ref.child(this.userDetails.user.uid)
+			$firebaseObject(profileRef).$save().then((response) => {
+				console.log("saved?");
+				console.log(response)
+			}).catch((error) => {
+				console.log("error?");
+				console.log(error);
+			})*/
+		}
 	}
 
 
