d2b551e90821de978d01e914dc5531098c271a26
Ben S <allobon@gmail.com>
2016-08-07 15:17:09 +0100

    basic boilerplate

---
+++ client/js/src/main.js
@@ -0,0 +1,11 @@
+import angular from "angular";
+
+
+angular.module("angularfireApp", [])
+	.controller("mainController", mainController);
+
+
+
+function mainController(){
+	console.log("nothing to bind")
+}
